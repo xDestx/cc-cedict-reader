281bc6bf45b2a16875dcf8aedf90d255d648bf84
xDestx <destroyer7712@gmail.com>
2026-01-30 00:53:43 -0500

    simplify to get something running

--- cccedict.go
+++ cccedict.go
@@ -2,182 +2,59 @@ package cccedictparser
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 )
 
-type tone = int
-
-const (
-	_ tone = iota
-	Yi
-	Er
-	San
-	Si
-	Wu
-)
-
-type PinyinSyllable struct {
-	Romanization string
-	Tone         tone
-}
-
-type PinyinChunk = []PinyinSyllable
-
 type Ci struct {
 	Fantizi  string
 	Jiantizi string
-	Pinyin   []PinyinChunk
+	Pinyin   string
 	Gloss    []string
 }
 
-const parseLineRegex = `^([\p{Han}，·]+) ([\p{Han}，·]+) \[\[?((?:(?:\w+\d)+\s*,?\s*)+)\]\]? \/(.+)\/$`
-const pinyinRegex = `([a-zA-Z]+)([12345])`
+const pinyinRegex = `\[\[?((?:(?:\w+:?·?\d?)+\s*,?·?\s*)+)\]\]?`
 
-func pinyinChunkFromStr(pyChunkStr string) (PinyinChunk, error) {
-	r, err := regexp.Compile(pinyinRegex)
-
-	m := r.FindAllStringSubmatch(pyChunkStr, -1)
-
-	if err != nil {
-		return []PinyinSyllable{}, err
+func joinAllStrings(s []string, joinStr string) string {
+	var sb strings.Builder
+	for i := range s {
+		sb.WriteString(s[i])
+		sb.WriteString(joinStr)
 	}
 
-	pinyinYiGes := []PinyinSyllable{}
-	var actualStrBuilder strings.Builder
-	for vArr := range m {
-		t, err := StringToTone(m[vArr][2])
-
-		if err != nil {
-			return []PinyinSyllable{}, fmt.Errorf("error parsing pinyin for (%s) error: %s", pyChunkStr, err.Error())
-		}
-
-		newPy := PinyinSyllable{
-			Romanization: m[vArr][1],
-			Tone:         t,
-		}
-
-		pinyinYiGes = append(pinyinYiGes, newPy)
-
-		actualStrBuilder.WriteString(PinyinSyllableString(newPy))
-	}
-
-	actualStr := actualStrBuilder.String()
-
-	if pyChunkStr != actualStr {
-		return []PinyinSyllable{}, fmt.Errorf("mismatch between parsed and actual value. Expected (%s) Was (%s)", pyChunkStr, actualStr)
-	}
-
-	return pinyinYiGes, nil
-}
-
-func PinyinFromString(pystr string) ([]PinyinChunk, error) {
-	pinyins := strings.Split(pystr, " ")
-
-	py := []PinyinChunk{}
-
-	for i := range pinyins {
-		v, err := pinyinChunkFromStr(pinyins[i])
-
-		if err != nil {
-			return [][]PinyinSyllable{}, fmt.Errorf("error parsing pinyin for (%s). %s", pystr, err)
-		}
-
-		py = append(py, v)
-	}
-
-	return py, nil
+	return sb.String()
 }
 
 // Traditional Simplified [[pinb1yin1]] /gloss; gloss; .../gloss; gloss; .../
 func ParseLine(line string) (Ci, error) {
-	r, err := regexp.Compile(parseLineRegex)
+	items := strings.Split(line, " ")
+	r, err := regexp.Compile(pinyinRegex)
+
+	pinyinAndGlossStr := joinAllStrings(items[2:], " ")
+	pinyinGlossSplit := strings.Split(pinyinAndGlossStr, " /")
+	pinyinOnly := pinyinGlossSplit[0]
+	fullGloss := joinAllStrings(pinyinGlossSplit[1:], " /")
+	fullGlossStripped := fullGloss[0:][:len(fullGloss)-4]
+	g := strings.Split(fullGlossStripped, "/")
 
 	if err != nil {
 		return Ci{}, err
 	}
 
-	s := r.FindStringSubmatch(line)
+	pinyinMatch := r.FindStringSubmatch(pinyinOnly)
 
-	if len(s) == 0 {
-		return Ci{}, errors.New("error parsing line: " + line)
+	if len(pinyinMatch) == 0 {
+		return Ci{}, errors.New("error parsing pinyin for line: " + line)
 	}
 
-	py, err := PinyinFromString(s[3])
-
-	if err != nil {
-		return Ci{}, err
-	}
-
-	gloss := strings.Split(s[4], "/")
+	t := items[0]
+	s := items[1]
+	p := pinyinMatch[1]
 
 	return Ci{
-		Fantizi:  s[1],
-		Jiantizi: s[2],
-		Pinyin:   py,
-		Gloss:    gloss,
+		Fantizi:  t,
+		Jiantizi: s,
+		Pinyin:   p,
+		Gloss:    g,
 	}, nil
-}
-
-func ToneToString(t tone) string {
-	switch t {
-	case Yi:
-		return "1"
-	case Er:
-		return "2"
-	case San:
-		return "3"
-	case Si:
-		return "4"
-	case Wu:
-		return "5"
-	default:
-		return "?"
-	}
-}
-
-func StringToTone(s string) (tone, error) {
-	switch s {
-	case "1":
-		return Yi, nil
-	case "2":
-		return Er, nil
-	case "3":
-		return San, nil
-	case "4":
-		return Si, nil
-	case "5":
-		return Wu, nil
-	default:
-		return 0, errors.New("unable to identify tone " + s)
-	}
-}
-
-func PinyinSyllableString(p PinyinSyllable) string {
-	return p.Romanization + ToneToString(p.Tone)
-}
-
-func PinyinChunkString(py PinyinChunk) string {
-	var r strings.Builder
-
-	for i := 0; i < len(py); i++ {
-		r.WriteString(PinyinSyllableString(py[i]))
-	}
-
-	return r.String()
-}
-
-func PinyinString(p []PinyinChunk) string {
-	var r strings.Builder
-	l := len(p)
-
-	for i := range l {
-		r.WriteString(PinyinChunkString(p[i]))
-		if (i + 1) < l {
-			r.WriteString(" ")
-		}
-	}
-
-	return r.String()
 }

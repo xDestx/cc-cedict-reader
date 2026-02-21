f23dc136d8dd3673f080fb01a2bc0c726155811a
xdest <destroyer7712@gmail.com>
2026-02-20 22:54:28 -0500

    Add cmd

---
+++ cmd/cc-cedict-parse/cc-cedict-parse.go
@@ -0,0 +1,47 @@
+package cccedictparse
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+
+	cccedictparser "github.com/xDestx/cc-cedict-reader"
+)
+
+func main() {
+	var input io.Reader
+	if len(os.Args) == 2 {
+		// file
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer f.Close()
+		input = f
+	} else if len(os.Args) == 1 {
+		input = os.Stdin
+	} else {
+		log.Fatalf("format: <cmd> <optional file path>\nEx: cc-cedict-parse\nEx: cc-cedict-parse path/to/my/file")
+		return
+	}
+
+	scanner := bufio.NewScanner(input)
+
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		l := scanner.Text()
+
+		ci, err := cccedictparser.ParseLine(l)
+
+		if err == nil {
+			os.Stdout.WriteString(ci.String() + "\n")
+		} else {
+			os.Stdout.WriteString(err.Error())
+		}
+	}
+}
---
+++ cmd/cc-cedict-parse/go.mod
@@ -0,0 +1,9 @@
+module github.com/xDestx/cc-cedict-reader/cmd/cc-cedict-parse
+
+go 1.25.6
+
+require github.com/xDestx/cc-cedict-reader v0.0.0
+
+require golang.org/x/text v0.34.0 // indirect
+
+replace github.com/xDestx/cc-cedict-reader v0.0.0 => ../../
